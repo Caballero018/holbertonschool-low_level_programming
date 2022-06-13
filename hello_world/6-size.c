258eea225a8a7aa9746be0d70b8ca7bc20f6f4ea
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-13 13:15:10 -0700

    Printf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
+ * main - size of memory
  *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	printf("Size of a char: 1 byte(s)\n");
@@ -10,5 +13,6 @@ int main(void)
 	printf("Size of a long int: 8 byte(s)\n");
 	printf("Size of a long long int: 8 byte(s)\n");
 	printf("Size of a float: 4 byte(s)\n");
+
 	return (0);
 }
