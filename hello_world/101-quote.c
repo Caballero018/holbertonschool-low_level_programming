5b00ff56614223e8bc79ee37155d78dde7638f02
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-13 21:56:41 -0700

    fput

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - cout
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
