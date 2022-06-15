2eb2ed29f9d567ff5a7130412d74d0ad26d420f1
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 15:37:02 -0700

    printf numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - One digit of base 10
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int x;
+	
+	for(x=0; x <= 9; x++ )
+		printf("%d", x);
+
+	return (0);
+}
