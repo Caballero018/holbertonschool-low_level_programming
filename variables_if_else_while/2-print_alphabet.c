42db00c14e5bc4c1be61d6d49cad9f971a92743b
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 10:14:29 -0700

    putchar in while

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Command putchar in while
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char i = 'a';
+	char x = 'z';
+	
+	while (i <= x)
+	{
+		putchar(i);
+		putchar('\n');
+		i++;
+
+		return (0);
+	}
+}
+
+
