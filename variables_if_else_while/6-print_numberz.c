3d8b6bd2d6288a6848aef2fe08b8cce92f0f0424
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 16:13:16 -0700

    numbers of one digit with putchar

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,14 +8,13 @@
 
 int main(void)
 {
-        int x;
+	char x;
+	
+	for (x = 0 ; x < 10 ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
 
-        for (x = 0 ; x < 10 ; x++)
-        {
-                putchar(x);
-        }
-
-        putchar('\n');
-
-        return (0);
+	return (0);
 }
