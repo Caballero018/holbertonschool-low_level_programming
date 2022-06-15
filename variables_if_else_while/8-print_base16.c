ace7e272510520c44928ceceeb6e36613793e165
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 16:54:54 -0700

    Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -12,14 +12,11 @@ int main(void)
 	char y;
 
 	for (x = '0'; x <= '9' ; x++)
-	{
 		putchar(x);
 
-		for (y = 'a' ; y <= 'f'; y++)
-		{
-			putchar(y);
-		}
-	}
+	for (y = 'a' ; y <= 'f'; y++)
+		putchar(y);
+
 	putchar('\n');
 
 	return (0);
