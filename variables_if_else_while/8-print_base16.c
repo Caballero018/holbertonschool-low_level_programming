3557fa8e05ee738766ab7ee960accd4236f257e1
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 16:50:59 -0700

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Hexadecimal numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = '0'; x <= '9' ; x++)
+	{
+		putchar(x);
+
+		for (y = 'a' ; y <= 'f'; y++)
+			putchar(y)
+	}
+	putchar('\n');
+
+	return (0);
+
+}
