ff8bcb9287155618c1b9899eaa0a7899caaf04aa
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 16:35:53 -0700

    alphabet reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Command putchar in while
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char i = 'a';
+	char x = 'z';
+	
+	while (x >= i)
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n');
+	
+	return (0);
+}
