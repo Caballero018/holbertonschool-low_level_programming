122f59ef192b3775602335859f5c0f5aee11655a
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 16:09:14 -0700

    numbers of one digit with putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - One digit of base 10
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+        int x;
+
+        for (x = 0 ; x < 10 ; x++)
+        {
+                putchar(x);
+        }
+
+        putchar('\n');
+
+        return (0);
+}
