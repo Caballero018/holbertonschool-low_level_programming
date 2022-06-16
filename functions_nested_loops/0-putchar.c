93fc443259597138a3447abf03a2c901ddc9bc40
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-16 12:14:19 -0700

    putchar in main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<main.h>
 
 /**
  * main - print _putchar
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 
 	return (0);
 }
