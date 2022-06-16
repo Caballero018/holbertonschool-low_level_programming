269c77d0e1bc85f58e4f1be3c5da2e717057259b
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-15 17:25:06 -0700

    space and  signed dollar

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * main - of 0 and 9 with , and $
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+
+		if (i == '9')
+		{
+			putchar('$');
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		i++;
+	}
+
+	return (0);
+}
