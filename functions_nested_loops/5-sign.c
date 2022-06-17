6179de71348cd8e4fedd52cdce0ccfbb5a9c2ee7
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-17 10:39:39 -0700

    positive or negative

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,10 +1,14 @@
 #include "main.h"
 
 /**
+ * print_sign - Prints the sign depending
+ * on whether it is greater or less.
  *
+ * @n: Reflects the value according to its type with a sign
  *
+ * Return: Returns 1 if the number is positive, returns 0 if
+ * the number is equal to 0, and returns -1 if the number is negative
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
@@ -15,11 +19,11 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar('0');
-                return (0);
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
-                return (1);
+		return (1);
 	}
 }
