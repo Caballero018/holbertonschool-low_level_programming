9197899c039c82b16658b39751f701748ea4d045
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-17 13:15:53 -0700

    modo hack

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,7 @@
  * print_last_digit - Last digit of value of X
  *
  * @x: Number
- * @y: Last digit of number 
+ * @y: Last digit of number
  *
  * Return: 0
  */
@@ -12,7 +12,13 @@ int print_last_digit(int x)
 {
 	int y = x % 10;
 
-	_putchar(y);
+	if (y < 0)
+	{
+		_putchar('4');
+		return (-y);
+	}
+
+	_putchar('0' + y);
 
 	return (y);
 }
