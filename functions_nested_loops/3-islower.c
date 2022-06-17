8203fb2295f2235f7a5e7ddd4f6654c206a6866f
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-17 08:01:48 -0700

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * int _islower -
- *
- * Return: 1
- * Return: 0
+ * int _islower - Identify if they are lowercase with the islower function
+ * _islower: Identify of lowercase
+ * 
+ * Return: 1  if are lowercase and 0 if are cápital letters.
  */
 
 int _islower(int c)
 {
 	char c;
 
-	if(islower(c))
+	if (_islower(c))
 	{
 		_putchar(c);
 		return (1);
