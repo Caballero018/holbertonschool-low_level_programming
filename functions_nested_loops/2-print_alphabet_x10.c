3f9aff8b30d12964cf3b3e09a86fd67eaf47aa0a
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-16 20:21:02 -0700

    Alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,21 @@
 #include "main.h"
 
 /**
+ * print_alphabet_x10 - the alphabet multiplicate * 10
  *
+ * Return: 0
  *
  */
+void print_alphabet_x10(void)
+{
+	char x = 'a';
 
+	while (x <= 'z')
+	{
+		if(x == a)
+		{
+			_putchar(x * 10);
+			x++;			
+		}
+	}
+}
