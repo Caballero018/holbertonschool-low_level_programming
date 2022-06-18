58be4ef01f95b6089f6be00646acb00afb897a26
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-18 14:36:31 -0700

    putchar

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,15 +3,15 @@
 /**
  * int _islower - Identify if they are lowercase with the islower function
  * _islower: Identify of lowercase
- * 
+ *
  * Return: 1  if are lowercase and 0 if are cápital letters.
  */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	return(0);
+	return (0);
 }
