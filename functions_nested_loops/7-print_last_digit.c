14af317b91ef6b948b13522ebb6d4f472437917a
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-18 14:55:09 -0700

    putchar

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,21 @@
 #include "main.h"
-
 /**
- * print_last_digit - Last digit of value of X
- *
- * @x: Number
- *
- * Return: 0
+ * print_last_digit - function
+ * @n: variable of the function
+ * Return: n % 10
  */
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
-	int y = x % 10, r = x / 10;
-
-	if (y < 0)
-	{
-		return (-y);
-	}
-
-	_putchar('0' + r);
-	_putchar('0' + y);
-
-	return (y);
+int l;
+l = n % 10;
+if (l < 0)
+{
+_putchar(-l + 48);
+return (-l);
+}
+else
+{
+_putchar(l + 48);
+return (l);
+}
 }
