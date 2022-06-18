e589e6e4c07f5acf37e8fbce7df2fab54f603676
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-18 14:34:28 -0700

    putchar

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,14 @@
  */
 int print_last_digit(int x)
 {
-	int y = x % 10;
+	int y = x % 10, r = x / 10;
 
 	if (y < 0)
 	{
-		_putchar('4');
 		return (-y);
 	}
 
+	_putchar('0' + r);
 	_putchar('0' + y);
 
 	return (y);
