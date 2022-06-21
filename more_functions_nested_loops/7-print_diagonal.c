60e75c04ae0c7768eb0d2f999210a6142cb02fcd
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-21 09:00:19 -0700

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,7 +6,6 @@
  */
 void print_diagonal(int n)
 {
-	int x;
 
 	if (n <= 0)
 	{
@@ -14,11 +13,10 @@ void print_diagonal(int n)
 	}
 	else if (n > 0)
 	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar(' ');
-		}
+		_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
 	}
+	_putchar('\n');
 }
+
