9f70406c97a39b5aec45bea85b29be1f8cf76683
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-21 08:35:32 -0700

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
+ * print_diagonal - Print spaces and diagonal
  *
- *
+ * @x: Variable of the function
  */
 void print_diagonal(int n)
 {
@@ -13,7 +14,7 @@ void print_diagonal(int n)
 	}
 	else if (n > 0)
 	{
-		for (x = 0; x > n; x++)
+		for (x = 0; x < n; x++)
 		{
 			_putchar(' ');
 		}
