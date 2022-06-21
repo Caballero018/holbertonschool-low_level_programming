dad9aa5860f1e1d75ba4d27d442549f54bc57d82
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-20 21:00:28 -0700

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - line
+ */
+void print_line(int n)
+{
+	_putchar('\n');
+	_putchar('_');
+	_putchar('_');
+	_putchar('\n');
+
+	for (n = 0; n <= 10; n++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+	_putchar('\n');
+}
