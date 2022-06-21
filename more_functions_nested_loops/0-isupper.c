19a8eebb4cb81a3955bb858e4d99a16c692280a0
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-20 17:32:44 -0700

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _issuper - This function checks if the letter is uppercase or not
+ *
+ * @c: This variable helps us determine if the letters are uppercase
+ * Return: 1 is returned if it is uppercase and 0 if it is not.
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z' )
+	{
+		return (1);
+	}
+	return (0);
+}
