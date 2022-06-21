b6ba4c2738ece1835fde7e7cc0fd37907712c7a7
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-21 08:31:05 -0700

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,23 @@
 #include "main.h"
 /**
- * print_diagonal -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
  *
- * Return: Always 0.
+ *
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int x;
 
 	if (n <= 0)
-		_putchar('\n');
-	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j <= i; j++)
+		_putchar('\n');
+	}
+	else if (n > 0)
+	{
+		for (x = 0; x > n; x++)
 		{
-			if (i == j)
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
