167a1bb8aabb06c6589fd0db939a700a729f9d50
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-21 11:35:04 -0700

    print diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,17 +6,26 @@
  */
 void print_diagonal(int n)
 {
+	int x, i;
 
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else if (n > 0)
+	else
 	{
-		_putchar(' ');
-		_putchar('\\');
+		for (x = 0; x < n; x++)
+		{
+			_putchar(' ');
+			
+			for (i = 0; i < n; i++)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+		}
 		_putchar('\n');
+
 	}
-	_putchar('\n');
 }
 
