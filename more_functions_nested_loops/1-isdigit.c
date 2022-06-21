c7f75f45069c40393c9fa865a90987ba0db1b569
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-20 18:36:29 -0700

    isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
+ * _isdigit - function that differentiates between
+ * numbers 0 - 9 from other characters.
  *
+ * @c: is a variable that helps us differentiate between
+ * numbers 0 - 9 and from other characters.
  *
+ * Return: returns 1 if it is a number between (0 and 9),
+ * if it is something else it returns 0
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c <= 0 && c >= 9)
 	{
 		return (1);
 	}
