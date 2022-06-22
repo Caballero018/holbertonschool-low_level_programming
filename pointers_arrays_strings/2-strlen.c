9e3cca572e870b5009a25289f2ff7a4155d4ad0e
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-21 20:50:59 -0700

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: Variable that point char.
+ * Return: point
+ *
+ *
+ */
+int _strlen(char *s)
+{
+        return (*s);
+}
