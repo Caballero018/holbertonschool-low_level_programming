3d4e654857359315da26f8ba4edc909883564337
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-26 15:32:22 -0700

    strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
+ * _strcpy - Funtion that copy a string.
  *
+ * @dest: Array that copy the string.
+ * @src: String to copy to.
  *
+ * Return: dest.
  */
 char *_strcpy(char *dest, char *src)
 {
-        int i = 0;
+	int i = 0;
 
-        while (src[i] <= '\0')
-        {
-                dest[i] = src[i];
-                i++;
-        }
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
 	return (dest);
-
 }
