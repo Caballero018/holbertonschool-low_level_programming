0c52ec9b820527f8af706bd8e7ade4b6703444e9
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-27 11:32:37 -0700

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that adds a string to another string.
+ *
+ * @dest: Variable to which the string is added.
+ * @src: String to add.
+ *
+ * Return: (dest).
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, x = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[x] != '\0')
+	{
+		x++;
+	}
+
+	dest[i] = x;
+
+	return (dest);
+}
+
