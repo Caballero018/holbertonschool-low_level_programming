5b93a56ae3e1c545119ae137c58f3584f4a2b6ed
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-28 15:37:11 -0700

    concatenate two strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -6,13 +6,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 0, j = 0;
 
-	while (dest[i])
+
+	while (dest[i] != '\0')
 	{
-		dest[i] = src[n];
 		i++;
-		n++;
+	}
+	while (j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+
 	}
 
 	return (dest);
