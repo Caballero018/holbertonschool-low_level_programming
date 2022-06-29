cf04df0645213d0df7aa3eb98695f972f5b66ffd
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-28 18:43:35 -0700

    copy a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,27 @@
 #include "main.h"
 
 /**
+ * _strncpy - Copies the second string according to its number of bytes.
  *
+ * @dest: Copy to src.
+ * @src: String.
+ *
+ * Return: dest.
  *
  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
 
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
