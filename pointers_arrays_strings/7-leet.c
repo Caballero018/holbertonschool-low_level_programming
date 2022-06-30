1020d19f89b27b07cdc96270d804517db9075363
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-30 10:34:47 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337.
+ * leet - Replace the addresses of 'x' by those of 'y'
+ * in case 'n' finds them when traversing them.
  *
- * @n: Pointer type char.
- * Return: n
- *
+ * @n: Point to char.
+ * Return: n.
  */
 char *leet(char *n)
 {
+	char x[] = "aAeEoOtTlL";
+	char y[] = "4433007711";
 	int i, j;
-	char s1[] = "aAeEoOtTlL";
-	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
-			{
-				n[i] = s2[j];
-			}
+			if (n[i] == x[j])
+				n[i] = y[j];
 		}
 	}
 	return (n);
