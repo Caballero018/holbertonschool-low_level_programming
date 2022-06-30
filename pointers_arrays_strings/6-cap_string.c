ab0ef0e47e27dc15c66c41d23b1694e411404be0
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-06-29 19:07:49 -0700

    Make the first letter of a word capitalized

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
 /**
+ * cap_string - This function defines delimiters and after them
+ * the first letter of each word becomes uppercase.
+ *
+ * @n: points to the variable that is specified in main.
+ * Return: n.
  *
  */
 char *cap_string(char *n)
 {
 	int i = 0;
-	
+
 	while (n[i] != '\0')
 	{
-		if (n[i] == ' ' || n[i] == ',' || n[i] == '\n' || n[i] == ';' || n[i] == '"' || n[i] == '\t' || n[i] == '.' || n[i] == '!' || n[i] == '?' || n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
+		if (n[i] == ' ' || n[i] == ',' || n[i] == '\n' || n[i] == ';' ||
+		    n[i] == '"' || n[i] == '\t' || n[i] == '.' || n[i] == '!' ||
+		    n[i] == '?' || n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
 		{
 			if (n[1 + i] >= 'a' && n[1 + i] <= 'z')
 				n[1 + i] = n[1 + i] - 32;
