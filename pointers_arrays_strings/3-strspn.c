75258936418a524d5798d6660afe032e362f615e
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-02 14:43:28 -0700

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,18 @@
 #include "main.h"
+
 /**
- * _strspn - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ *
+ *
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+	unsigned int i = 0;
 
-	while (*s)
+	while (s[i] != '\0' && accept[i] != '\0')
 	{
-		for (r = 0; accept[r]; r++)
-		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
-		}
-		s++;
+		if (s[i] == accept[i])
+			return (i);
+		i++;
 	}
-	return (n);
+	return (i);
 }
