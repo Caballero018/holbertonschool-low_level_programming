d6b82f12e77713c8f390c8536b31adbe6279ff34
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-06 09:17:21 -0700

    name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Name
+ *
+ * Return: 0 always.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
