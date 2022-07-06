e3d12be8550714b51a70dc787c25724fe3837b60
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-06 10:25:52 -0700

    Prints all arguments it receives

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -10,9 +10,9 @@
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int i = 0;
-	
+
 	for (i = 0; i < argc; i++)
-			printf("%s\n", argv[i]);
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
