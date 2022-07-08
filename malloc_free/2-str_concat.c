0afde9de6551c2d31c7d4cee4decb8339b657b53
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-07 20:10:22 -0700

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,10 +15,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i])
+	while (s1[i] != '\0')
 		i++;
 
-	while (s1[i])
+	while (s1[i] != '\0')
 		j++;
 
 	x = malloc(sizeof(char) * (i + j + 1));
@@ -26,12 +26,13 @@ char *str_concat(char *s1, char *s2)
 	if (x == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		x[i] = s1[i];
 
-	for (j = 0; s2[j]; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		x[i++] = s2[j];
 
+	x = '\0';
 
 	return (x);
 	free (x);
