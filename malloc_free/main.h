760f6dd3369493cdb196f2853c4a3c7454a482bd
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-07 20:59:09 -0700

    strtow

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MALLOC_FREE
 
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
@@ -9,6 +10,7 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int _putchar(char c);
-
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
