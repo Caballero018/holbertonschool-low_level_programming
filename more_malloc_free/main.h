b08016af9c2348db7c9c2796526a84b4683063e6
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-10 20:52:32 -0700

    C - More malloc, free

---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MORE_Malloc
+#define MORE_Malloc
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+int _putchar(char c);
+
+#endif
