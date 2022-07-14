36c5891b91cc82d64b9ce12a3577ac7a380451d1
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-13 19:11:02 -0700

    Function that stores a copy of the name and owner.

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - Function that stores a copy of the name and owner.
+ *
+ * @name: Copy of name of dog.
+ * @age: Age of dog.
+ * @owner: Copy of name of owner of dog.
+ * Return: d
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int nlon = 0, olon = 0, i; /*n'lon'(diminutive of longitude) */
+	dog_t *d;			/* and o'lon'(diminutive of longitude) */
+
+	d = (dog_t *)malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		return (NULL);
+	while (name[nlon])
+		nlon++;
+	while (owner[olon])
+		olon++;
+
+	d->name = malloc(sizeof(d->name) * nlon + 1); /* Sufficient copy space */
+	if (d->name == NULL)				/* is reserved dynamically */
+	{
+		free(d->name);
+		return (NULL);
+	}
+	for (i = 0; i < nlon; i++) /* The copy of d->name was created */
+		d->name[i] = d->name[i];
+
+	d1->age = age;
+
+	d->owner = malloc(sizeof(d->owner) * olen); /* Sufficient copy space */
+	if (d->owner == NULL)				/* is reserved dynamically */
+	{
+		free(d->owner);
+		return (NULL);
+	}
+	for (i = 0; i < olon; i++) /* The copy of d->owner was created */
+		d->owner[i] = d->owner[i];
+
+	return (d);
+	free(d);
+
+}
