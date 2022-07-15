b038190a6bc1d6f892bc8ac50653e9c6fe385c2a
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-07-15 10:22:56 -0700

    Create a print

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,13 @@
 #include "function_pointers.h"
-#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
- **/
+ * print_name - Print name.
+ *
+ * @name: Variable to print.
+ * @f: Fuction pointer.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
 	f(name);
 }
