64179ce06c3984fe0c21b44e054983fd84649b15
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-08-02 16:10:24 -0700

    as

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - Function that prints a name.
  *
- * @name: Name of the person.
+ * @name: Name of the erson.
  * @f: Function pointer.
  */
 void print_name(char *name, void (*f)(char *))
