414a948cce0d5d767f49cc807a0c8dfef63d1101
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-08-02 16:09:34 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-print_name.c -o a

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,12 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - Function that prints a name.
  *
- * @name: Variable to print.
- * @f: Fuction pointer.
+ * @name: Name of the person.
+ * @f: Function pointer.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+	(f)(name);
 }
