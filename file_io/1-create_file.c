13865d2637186f5d95f9c5ce7a0d281983a942e3
Caballero018 <alejoxcaballero18@gmail.com.com>
2022-08-03 17:51:46 -0700

    Function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,8 +14,6 @@ int create_file(const char *filename, char *text_content)
 {
 	int cre, i = 0, wr;
 
-	if (cre == -1)
-		return (-1);
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
@@ -24,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	cre = open(filename, O_RDWR, 0600);
 	wr = write(cre, text_content, i);
-	if (wr == -1)
+	if ((wr == -1 || cre == -1))
 		return (-1);
 	close(cre);
 	return (1);
